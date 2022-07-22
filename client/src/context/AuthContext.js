737e498ec48979323b13c3eb0a923a06e6a24636
CoGuisMod <coguismod@gmail.com>
2022-07-21 19:01:21 -0500

    Initial setup, firebase conection.

---
+++ client/src/context/AuthContext.js
@@ -0,0 +1,114 @@
+/* Context Imports */
+import { createContext, useContext, useEffect, useState } from "react";
+
+/* Firebase config Imports */
+import { firebaseFirestore } from "../firebase/config";
+
+/* Firestore Imports */
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+} from "firebase/firestore";
+
+/* General Imports */
+import { useNavigate } from "react-router-dom";
+
+const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [users, setUsers] = useState(null);
+  const [message, setMessage] = useState("");
+
+  const [updateUsers, setUpdateUsers] = useState(false);
+
+  const navigate = useNavigate();
+
+  const signUp = async (email, password, rol, firstName, lastName) => {
+    const docuRef = doc(firebaseFirestore, "users", email);
+    await setDoc(docuRef, {
+      email: email,
+      password: password,
+      rol: rol,
+      first_name: firstName,
+      last_name: lastName,
+    });
+  };
+
+  const logIn = async (email, password) => {
+    const docuRef = doc(firebaseFirestore, `users/${email}`);
+    const initialData = await getDoc(docuRef);
+    const finalData = initialData.data();
+    if (finalData.password !== password) {
+      setMessage("Contraseña incorrecta");
+      return;
+    }
+    if (finalData.password === password) {
+      setUser(finalData);
+      window.localStorage.setItem("user", JSON.stringify(finalData));
+      if (finalData.rol === "seller") {
+        navigate("/seller");
+      }
+      if (finalData.rol === "mechanic") {
+        navigate("/mechanic");
+      }
+      if (finalData.rol === "admin") {
+        navigate("/admin");
+      }
+    } else {
+      return;
+    }
+  };
+
+  const logOut = () => {
+    setUser(null);
+    localStorage.clear();
+    navigate("/");
+  };
+
+  const deleteUser = async (email) => {
+    const docuRef = doc(firebaseFirestore, `users`, email);
+    await deleteDoc(docuRef);
+  };
+
+  const getUsers = async () => {
+    const docuRef = collection(firebaseFirestore, "users");
+    const initialData = await getDocs(docuRef);
+    const finalData = initialData.docs.map((doc) => doc.data());
+    setUsers(finalData);
+  };
+
+  useEffect(() => {
+    setUser(JSON.parse(window.localStorage.getItem("user")));
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        signUp,
+        logIn,
+        logOut,
+        user,
+        setUser,
+        deleteUser,
+        getUsers,
+        users,
+        setUsers,
+        message,
+        setMessage,
+        updateUsers,
+        setUpdateUsers,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export function UserAuth() {
+  return useContext(AuthContext);
+}
---
+++ client/src/firebase/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDGdB3TiJ_zWmn4CRrpC2LymBCyclgc2dk",
+  authDomain: "ie-mm-a2c59.firebaseapp.com",
+  projectId: "ie-mm-a2c59",
+  storageBucket: "ie-mm-a2c59.appspot.com",
+  messagingSenderId: "883591730783",
+  appId: "1:883591730783:web:cf7780cf3d295e3d3eedea",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const firebaseFirestore = getFirestore(firebaseApp);
+
+export { firebaseApp, firebaseFirestore };
